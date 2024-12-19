9ef499ae311a537016a85c5c37cd821650ee0077
Maria da Conceição Pereira Tavares <mariatavares@MacBook-Pro-de-Maria.local>
2024-12-19 13:12:30 +0000

    CreateDjango Models (Car Model and Car Make Model)

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -14,4 +14,6 @@ urlpatterns = [
     path(route="logout", view=views.logout_request, name="logout"),
     # path for dealer reviews view
     # path for add a review view
+    # path for get cars
+    path(route="get_cars", view=views.get_cars, name="get_cars"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
