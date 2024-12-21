75989241dbbbff9df223ac428854483dbbeb667c
Maria da Conceição Pereira Tavares <mariatavares@MacBook-Pro-de-Maria.local>
2024-12-21 18:02:10 +0000

    Add Post Review Page [Sentimental is not working]

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -4,6 +4,7 @@ import LoginPanel from "./components/Login/Login"
 import RegisterPanel from "./components/Register/Register";
 import DealersPanel from "./components/Dealers/Dealers"
 import DealerPanel from "./components/Dealers/Dealer"
+import PostReview from "./components/Dealers/PostReview"
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Route path="/register" element={<RegisterPanel />} />
       <Route path="/dealers" element={<DealersPanel />} />
       <Route path="/dealer/:id" element={<DealerPanel />} />
+      <Route path="/postreview/:id" element={<PostReview/>} />
     </Routes>
   );
 }
